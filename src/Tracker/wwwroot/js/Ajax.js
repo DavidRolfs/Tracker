253e05dc49b529ced35922ff3aa0070ab5200734
david rolfs <davidrolfs15@gmail.com>
2017-08-17 10:00:01 -0700

    add ajax to display form on Associates indexpage

---
+++ src/Tracker/wwwroot/js/Ajax.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('.display-view').click(function () {
+        $.ajax({
+            type: 'GET',
+            dataType: 'html',
+            url: '/Associate/Create',
+            success: function (result) {
+                $('#result4').html(result);
+            }
+        });
+    });
+});
